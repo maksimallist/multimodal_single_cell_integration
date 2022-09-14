a0ac7545a45afa033104a6d9daf19ea4f52bab88
mks <maksimallist@gmail.com>
2022-09-14 12:08:27 +0300

    ref: flow dataset return not only tensors but cell_id too;

--- src/dataset.py
+++ src/dataset.py
@@ -8,7 +8,8 @@ from torch.utils.data import Dataset
 
 
 class FlowDataset(Dataset):
-    _reserve_name: str = "block0_values"
+    _features_name: str = "block0_values"
+    _cell_id_name: str = "axis1"
 
     def __init__(self,
                  features_file: str,
@@ -19,9 +20,11 @@ class FlowDataset(Dataset):
         self.features, self.features_names, self.features_shape = self.get_hdf5_flow(features_file)
         if targets_file:
             self.targets, self.targets_names, self.targets_shape = self.get_hdf5_flow(targets_file)
-            assert self.targets_shape[0] == self.features_shape[0]
+            assert self.targets_shape[0] == self.features_shape[0], \
+                AssertionError(f"Длины файлов фичей и таргетов не совпадают; "
+                               f"features_file: {self.features_shape[0]}, targets_file: {self.targets_shape[0]};")
         else:
-            self.targets = targets_file
+            self.targets = None
 
         self.device = device
         self.transform = transform
@@ -30,9 +33,10 @@ class FlowDataset(Dataset):
     def get_hdf5_flow(self, file_path: str):
         file_flow = h5py.File(file_path, 'r')
         col_names = list(file_flow[list(file_flow.keys())[0]])
-        assert self._reserve_name in col_names
+        assert self._features_name in col_names
+        assert self._cell_id_name in col_names
 
-        lines, features_shape = file_flow[list(file_flow.keys())[0]]["block0_values"].shape
+        lines, features_shape = file_flow[list(file_flow.keys())[0]][self._features_name].shape
         data_flow = file_flow[list(file_flow.keys())[0]]
 
         return data_flow, col_names, (lines, features_shape)
@@ -40,10 +44,14 @@ class FlowDataset(Dataset):
     def __len__(self):
         return len(self.features['axis1'])
 
-    def __getitem__(self, item: int) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
+    def __getitem__(self, item: int) -> Union[Tuple[str, torch.Tensor], Tuple[str, torch.Tensor, torch.Tensor]]:
+        x_cell_id = self.features[self._cell_id_name][item].decode("utf-8")
         if self.targets is not None:
-            x = torch.from_numpy(self.features['block0_values'][item])
-            y = torch.from_numpy(self.targets['block0_values'][item])
+            y_cell_id = self.targets[self._cell_id_name][item].decode("utf-8")
+            assert x_cell_id == y_cell_id, AssertionError("Несовпадение порядка элементов в списке фичей и таргетов.")
+
+            x = torch.from_numpy(self.features[self._features_name][item])
+            y = torch.from_numpy(self.targets[self._features_name][item])
 
             if self.transform:
                 x = self.transform(x)
@@ -52,13 +60,13 @@ class FlowDataset(Dataset):
             if self.device:
                 x, y = x.to(self.device), y.to(self.device)
 
-            return x, y
+            return x_cell_id, x, y
         else:
-            x = torch.from_numpy(self.features['block0_values'][item])
+            x = torch.from_numpy(self.features[self._features_name][item])
 
             if self.transform:
                 x = self.transform(x)
             if self.device:
                 x = x.to(self.device)
 
-            return x
+            return x_cell_id, x
