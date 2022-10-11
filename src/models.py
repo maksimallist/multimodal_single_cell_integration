1f0ae7b35b6f653ca7cfe2caea3d45ddaa5cfd24
mks <maksimallist@gmail.com>
2022-10-12 00:39:05 +0300

    add: burtsev image forward model;

--- src/models.py
+++ src/models.py
@@ -360,3 +360,84 @@ class BurtsevAutoEncoder(nn.Module):
         x = x.flatten(start_dim=1)
 
         return self.activation(self.enc_neck(x))
+
+
+@dataclass
+class BurtsevForwardModelConf:
+    input_shape: Tuple[int, int, int]
+    activation: Callable
+    scale = 2
+    in_channels = [1, 4, 8, 16, 32, 64, 64, 64, 128, 256, 256, 256, 256, 128, 128, 64, 64, 64, 32, 16, 8, 4]
+    out_channels = [4, 8, 16, 32, 64, 64, 64, 128, 256, 256, 256, 256, 128, 128, 64, 64, 64, 32, 16, 8, 4, 1]
+    kernels = [7, 7, 7, 7, 7, 7, 7, 7, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 3, 3, 3, 3]
+    before_pool = {7: 1}
+    after_pool = {7: 7, 5: 9, 3: 5}
+
+
+class BurtsevForwardModel(nn.Module):
+    def __init__(self, conf: BurtsevForwardModelConf):
+        super(BurtsevForwardModel, self).__init__()
+        self.activation = conf.activation
+        self.conv0 = nn.Conv2d(conf.in_channels[0], conf.out_channels[0], conf.kernels[0])
+        self.pool = nn.AvgPool2d(conf.scale)
+        # tower
+        self.conv1 = nn.Conv2d(conf.in_channels[1], conf.out_channels[1], conf.kernels[1])
+        self.conv2 = nn.Conv2d(conf.in_channels[2], conf.out_channels[2], conf.kernels[2])
+        self.norm1 = nn.BatchNorm2d(conf.out_channels[2])
+        self.conv3 = nn.Conv2d(conf.in_channels[3], conf.out_channels[3], conf.kernels[3])
+        self.conv4 = nn.Conv2d(conf.in_channels[4], conf.out_channels[4], conf.kernels[4])
+        self.norm2 = nn.BatchNorm2d(conf.out_channels[4])
+        self.conv5 = nn.Conv2d(conf.in_channels[5], conf.out_channels[5], conf.kernels[5])
+        self.conv6 = nn.Conv2d(conf.in_channels[6], conf.out_channels[6], conf.kernels[6])
+        self.norm3 = nn.BatchNorm2d(conf.out_channels[6])
+        self.conv7 = nn.Conv2d(conf.in_channels[7], conf.out_channels[7], conf.kernels[7])
+        self.conv8 = nn.Conv2d(conf.in_channels[8], conf.out_channels[8], conf.kernels[8])
+        self.norm4 = nn.BatchNorm2d(conf.out_channels[8])
+        self.conv9 = nn.Conv2d(conf.in_channels[9], conf.out_channels[9], conf.kernels[9])
+        self.conv10 = nn.Conv2d(conf.in_channels[10], conf.out_channels[10], conf.kernels[10])
+        self.norm5 = nn.BatchNorm2d(conf.out_channels[10])
+        self.conv11 = nn.Conv2d(conf.in_channels[11], conf.out_channels[11], conf.kernels[11])
+        self.conv12 = nn.Conv2d(conf.in_channels[12], conf.out_channels[12], conf.kernels[12])
+        self.norm6 = nn.BatchNorm2d(conf.out_channels[12])
+        self.conv13 = nn.Conv2d(conf.in_channels[13], conf.out_channels[13], conf.kernels[13])
+        self.conv14 = nn.Conv2d(conf.in_channels[14], conf.out_channels[14], conf.kernels[14])
+        self.norm7 = nn.BatchNorm2d(conf.out_channels[14])
+        self.conv15 = nn.Conv2d(conf.in_channels[15], conf.out_channels[15], conf.kernels[15])
+        self.conv16 = nn.Conv2d(conf.in_channels[16], conf.out_channels[16], conf.kernels[16])
+        self.norm8 = nn.BatchNorm2d(conf.out_channels[16])
+        self.conv17 = nn.Conv2d(conf.in_channels[17], conf.out_channels[17], conf.kernels[17])
+        self.conv18 = nn.Conv2d(conf.in_channels[18], conf.out_channels[18], conf.kernels[18])
+        self.norm9 = nn.BatchNorm2d(conf.out_channels[18])
+        self.conv19 = nn.Conv2d(conf.in_channels[19], conf.out_channels[19], conf.kernels[19])
+        self.conv20 = nn.Conv2d(conf.in_channels[20], conf.out_channels[20], conf.kernels[20])
+        self.norm10 = nn.BatchNorm2d(conf.out_channels[20])
+
+        self.conv21 = nn.Conv2d(conf.in_channels[21], conf.out_channels[21], conf.kernels[21])
+
+    def forward(self, tensor: torch.Tensor) -> torch.Tensor:
+        x = self.activation(self.conv0(tensor))
+        x = self.pool(x)
+        # tower
+        x = self.activation(self.conv1(x))
+        x = self.activation(self.norm1(self.conv2(x)))
+        x = self.activation(self.conv3(x))
+        x = self.activation(self.norm2(self.conv4(x)))
+        x = self.activation(self.conv5(x))
+        x = self.activation(self.norm3(self.conv6(x)))
+        x = self.activation(self.conv7(x))
+        x = self.activation(self.norm4(self.conv8(x)))
+        x = self.activation(self.conv9(x))
+        x = self.activation(self.norm5(self.conv10(x)))
+        x = self.activation(self.conv11(x))
+        x = self.activation(self.norm6(self.conv12(x)))
+        x = self.activation(self.conv13(x))
+        x = self.activation(self.norm7(self.conv14(x)))
+        x = self.activation(self.conv15(x))
+        x = self.activation(self.norm8(self.conv16(x)))
+        x = self.activation(self.conv17(x))
+        x = self.activation(self.norm9(self.conv18(x)))
+        x = self.activation(self.conv19(x))
+        x = self.activation(self.norm10(self.conv20(x)))
+
+        out = self.activation(self.conv21(x))
+        return out
